aaea67179145bd0fc4c9300e4669424e3a3b6971
Gustavo Passos <gustavofsp@hotmail.com>
2023-04-24 16:27:47 -0300

    Add Authentication Configuration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
+using System.Text;
+using MinimalApiAuth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace MinimalApiAuth;
+
+
+        public static class Settings
+    {
+        public static string Secret = "41a75bd0-e2b4-11ed-b5ea-0242ac120002";
+    }
