02688e5fc0ea63dd5703b5488f391a45382aaafb
Gustavo Passos <gustavofsp@hotmail.com>
2023-04-24 19:08:42 -0300

    Creating Model, Repository, Service and endpoints

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using System.Security.Claims;
 using System.Text;
 using MinimalApiAuth;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using MinimalApiAuth.Models;
+using MinimalApiAuth.Repositories;
+using MinimalApiAuth.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,11 +25,56 @@ builder.Services.AddAuthentication(x =>
             ValidateIssuerSigningKey = true,
             IssuerSigningKey = new SymmetricSecurityKey(key),
             ValidateIssuer = false,
-            ValidateAudience = false
+            ValidateAudience = false // teste
         };
     });
-var app = builder.Build();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Admin", policy => policy.RequireRole("manager"));
+    options.AddPolicy("Employee", policy => policy.RequireRole("employee"));
+});
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+app.UseAuthentication(); // has to be in this order due th pipeline of the application
+app.UseAuthorization();
+
+app.MapPost("/login", (User model) =>
+{
+    var user = UserRepository.Get(model.UserName, model.Password);
+    if (user == null)
+    {
+        return Results.NotFound(new { message = "Usuario ou senha invalidos" });
+    }
+
+    var token = TokenService.GenerateToken(user);
+    user.Password = "";
+
+    return  Results.Ok( new 
+    {
+        user = user,
+        token = token
+
+    });
+});
+
+app.MapGet("/anonymous", () => Results.Ok("anonymous")).AllowAnonymous();
+
+app.MapGet("/authenticated", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new { message = $"Authenticated as {user.Identity.Name}" });
+    
+}).RequireAuthorization();
+
+app.MapGet("/employee", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new { message = $"Authenticated as {user.Identity.Name}" });
+    
+}).RequireAuthorization("Employee");
+
+app.MapGet("/manager", (ClaimsPrincipal user) =>
+{
+    Results.Ok(new { message = $"Authenticated as {user.Identity.Name}" });
+    
+}).RequireAuthorization("Admin");
 
 app.Run();
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MinimalApiAuth.Models;
+
+namespace MinimalApiAuth.Repositories
+{
+    public static class UserRepository
+    {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>()
+            {
+                new User() { Id = 1, UserName = "batman", Password = "batman", Role = "manager" },
+                new User() { Id = 2, UserName = "robin", Password = "robin", Role = "employee" }
+            };
+            return users.FirstOrDefault(x =>
+                string.Equals(x.UserName, username, StringComparison.CurrentCultureIgnoreCase) 
+                && x.Password.ToLower() == password.ToLower());
+        }
+
+    }
+}
